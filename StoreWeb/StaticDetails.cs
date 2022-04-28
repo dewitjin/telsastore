eb1bd3fd895769c210b6c176f19d4bba1146d9b3
Dewi Tjin <dewi.tjin@gmail.com>
2022-04-28 00:09:09 -0700

    Update StaticDetails.cs

--- StoreWeb/StaticDetails.cs
+++ StoreWeb/StaticDetails.cs
@@ -7,7 +7,7 @@ namespace StoreWeb
 {
     public static class StaticDetails
     {
-        public static string APIBaseUrl = "https://localhost:44316/";
+        public static string APIBaseUrl = "https://localhost:8080/"; //Note: this is set to current web api docker port. Change when testing.
         public static string CustomerUrl = $"{APIBaseUrl}api/customers/";
         public static string ItemUrl = $"{APIBaseUrl}api/items/";
         public static string OrderUrl = $"{APIBaseUrl}api/orders/";
