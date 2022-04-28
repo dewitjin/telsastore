32b2c229efaebdb1fa8edf4988a94237d43156e6
Dewi Tjin <dewi.tjin@gmail.com>
2022-04-28 16:17:02 -0700

    Revert "clean up using statements"
    This reverts commit d8e4d7ab88296556accddaad98677bbe681478b3.

--- StoreAPI/Startup.cs
+++ StoreAPI/Startup.cs
@@ -1,10 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using StoreAPI.Data;
 using StoreAPI.Helper;
 using StoreAPI.Repository;
